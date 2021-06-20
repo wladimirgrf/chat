fa29b99f18e34affc37db3b9bdca786452485686
Wladimir <wladimir.code@gmail.com>
2021-06-20 14:43:03 -0300

    feat: client websocket

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -1,7 +1,46 @@
 import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
+import { UsersService } from '../services/UsersService';
+import { MessagesService } from '../services/MessagesService';
+
+interface IParams {
+  text: string;
+  email: string;
+}
 
 io.on('connect', socket => {
-  socket.on('client_first_access', params => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
+
+  socket.on('client_first_access', async params => {
+    const socketId = socket.id;
+    const { text, email } = params as IParams;
+
+    let user = await usersService.findByEmail(email);
+
+    if (!user) {
+      user = await usersService.create(email);
+    }
+
+    let connectionId: string;
+    const userId = user.id;
+
+    const connection = await connectionsService.findByUserId(userId);
+
+    if (connection) {
+      connectionId = connection.id;
+    }
+
+    await connectionsService.create({
+      userId,
+      socketId,
+      id: connectionId,
+    });
+
+    await messagesService.create({
+      text,
+      userId,
+    });
   });
 });
